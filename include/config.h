be7a5bb805c08aabd40706f6288357f75f8c5e30
Oliver Seiler <oliver@seiler.kiwi>
2022-01-16 08:42:56 +1300

    Update pin#s

--- include/config.h
+++ include/config.h
@@ -13,25 +13,25 @@
 #define YELLOW_LED_PIN        26
 #define RED_LED_PIN           25
 
-//#define NEOPIXEL_PIN          4
-//#define NEOPIXEL_NUM          8
+//#define NEOPIXEL_PIN          32
+//#define NEOPIXEL_NUM          9
 
 //#define FEATHER_MATRIX_DATAPIN    27
 //#define FEATHER_MATRIX_CLOCKPIN   13
 
-//#define HUB75_R1 19
-//#define HUB75_G1 18
-//#define HUB75_B1 5
-//#define HUB75_R2 17
-//#define HUB75_G2 16
-//#define HUB75_B2 4
-//#define HUB75_CH_A 13
-//#define HUB75_CH_B 12
-//#define HUB75_CH_C 14
-//#define HUB75_CH_D 27
-//#define HUB75_CLK 26
-//#define HUB75_LAT 25
-//#define HUB75_OE 33
+//#define HUB75_R1 15
+//#define HUB75_G1 2
+//#define HUB75_B1 4
+//#define HUB75_R2 16
+//#define HUB75_G2 12
+//#define HUB75_B2 17
+//#define HUB75_CH_A 5
+//#define HUB75_CH_B 18
+//#define HUB75_CH_C 19
+//#define HUB75_CH_D 14
+//#define HUB75_CLK 27
+//#define HUB75_LAT 26
+//#define HUB75_OE 25
 
 #define I2C_CLK 50000     // SCD30 recommendation of 50kHz
 
