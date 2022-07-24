066478cdd280bfe2d873839100503a8f78f06687
Oliver Seiler <oliver@seiler.kiwi>
2022-07-24 20:43:48 +1200

    Updated git-rev.py for workflows

--- git-rev.py
+++ git-rev.py
@@ -1,23 +1,54 @@
 import subprocess
 import time
+import os
 
 # get tag/base version
-tag = subprocess.check_output("git describe --tags --abbrev=0", shell=True).decode().strip()
+try:
+  tag = subprocess.check_output("git describe --tags --abbrev=0", shell=True).decode().strip()
+except:
+  tag = "?"
+
+GITHUB_TAG = os.environ.get('GITHUB_TAG')
+print(GITHUB_TAG)
+
+if GITHUB_TAG is not None:
+  tag = GITHUB_TAG
+
 if tag.startswith('v'):
   tag = tag[1:]
 version = tag
 
 # get current revision hash
-commit = subprocess.check_output("git log --pretty=format:%h -n 1", shell=True).decode().strip()
+try:
+  commit = subprocess.check_output("git log --pretty=format:%h -n 1", shell=True).decode().strip()
+except:
+  commit = "?"
+
+GITHUB_SHA = os.environ.get('GITHUB_SHA')
+print(GITHUB_SHA)
+
+if GITHUB_SHA is not None:
+  commit = GITHUB_SHA
 
 # get branch name
-branch = subprocess.check_output("git rev-parse --abbrev-ref HEAD", shell=True).decode().strip()
+try:
+  branch = subprocess.check_output("git rev-parse --abbrev-ref HEAD", shell=True).decode().strip()
+except:
+  branch = "unknown"
+
+if GITHUB_TAG is not None:
+  branch = "main"
+
 # if not main branch append branch name
 if branch != "main":
   version += "-[" + branch + "]"
 
 # check if clean
-clean = subprocess.check_output("git status -uno --porcelain", shell=True).decode().strip()
+try:
+  clean = subprocess.check_output("git status -uno --porcelain", shell=True).decode().strip()
+except:
+  clean = ""
+
 ts = time.strftime('%Y%m%d%H%M%S')
 # if not clean append timestamp
 if clean != "":
