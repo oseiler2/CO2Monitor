95f4fefea1c527db5a0333ccda2e508b100a228a
Oliver Seiler <oseiler2@users.noreply.github.com>
2023-05-31 16:44:47 +1200

    ESP32-S3 support (#19)

--- include/hub75.h
+++ include/hub75.h
@@ -1,13 +1,18 @@
+#include <sdkconfig.h>
+
 #ifndef _HUB75_H
 #define _HUB75_H
 
 #include <Arduino.h>
-#include "globals.h"
+#include <globals.h>
 #include <config.h>
 #include <model.h>
+#include <Ticker.h>
+
+#if CONFIG_IDF_TARGET_ESP32
+
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 
-#include <Ticker.h>
 
 class HUB75 {
 public:
@@ -25,5 +30,18 @@ private:
   Ticker* cyclicTimer;
   boolean toggle;
 };
+
+#elif CONFIG_IDF_TARGET_ESP32S3
+
+class HUB75 {
+public:
+  HUB75(Model* _model);
+  ~HUB75();
+
+  void update(uint16_t mask, TrafficLightStatus oldStatus, TrafficLightStatus newStatus);
+  void stopDMA();
+};
+
+#endif
 
 #endif
